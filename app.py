c5a392f483ebb4f40b5ebaf8350d3ebf08eeefd2
Russel Abreo <russela@nseit.com>
2024-10-24 15:27:57 +0530

    changes

--- app.py
+++ app.py
@@ -6,10 +6,12 @@ import yaml
 from basicsr.utils.options import  parse_options
 from basicsr.models import build_model
 import cv2
+import numpy as np
 # from basicsr.utils import ordered_yaml
 
 def load_image(image_path):
     """ Load an image from the path and convert it to a tensor """
+    file_bytes = np.asarray(bytearray(image_path.read()), dtype=np.uint8)
     img = cv2.imread(image_path, cv2.IMREAD_COLOR)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     img = torch.from_numpy(img).float().permute(2, 0, 1).unsqueeze(0) / 255.0  # (1, C, H, W) in [0, 1]
@@ -60,7 +62,7 @@ if uploaded_file is not None:
 
     # Convert the output tensor to an image
     # output_image = transforms.ToPILImage()(output.squeeze())
-    output_image = transforms.ToPILImage()(model.get_current_visuals()['result'].squeeze())
+    output_image = transforms.ToPILImage()(model.get_current_visuals()['result'].squeeze().cpu())
 
 
     # Display the result
