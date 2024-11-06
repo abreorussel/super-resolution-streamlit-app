b3423bb832a7ccfdd5f7037a7d5f27801ead78b2
abreorussel <68015056+abreorussel@users.noreply.github.com>
2024-11-06 22:48:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-import streamlit as st
-import cv2                          # Add OpenCV for preprocessing and postprocessing
-from PIL import Image, ImageFilter
+# Import necessary packages
+import streamlit as st                      
+from PIL import Image
 import numpy as np
 from streamlit_drawable_canvas import st_canvas
 import torch
@@ -22,18 +22,18 @@ file_path = "/content/test_base_benchmark_x2.yml"
 opt = parse_options(file_path, is_train=False)
 model = build_model(opt)
 
-def preprocess_image(image):
-    # Directly return the original image without any preprocessing
-    return image
-
-def advanced_postprocess_image(image):
-    # Convert image to PIL format (if not already) and apply a mild sharpening filter
-    sharpened_image = image.filter(ImageFilter.UnsharpMask(radius=1, percent=100, threshold=1))
-    return sharpened_image
-
 # Streamlit app title and description
-# st.title(f"Image Upscaling with {model_selection}")
 st.title(f"Generating High Resolution Zoom-In for Images using LMLT-Base-x2")
+
+st.markdown(
+    """
+    <div style="text-align: center; background-color: ##000000; border-radius: 10px; padding: 10px;">
+        <h2><strong>Russel Abreo</strong> &nbsp;&nbsp; <strong>Anand Patel</strong></h2>
+        <h2>Team Name: Convolutioneers (24m2107, 24m2119)</h2>
+    </div>
+    """,
+    unsafe_allow_html=True
+)
 
 # File uploader
 uploaded_file = st.file_uploader("Upload an Image", type=["jpg", "png", "jpeg"])
@@ -46,21 +46,17 @@ if uploaded_file is not None:
     st.image(image, caption="Uploaded Image", use_column_width=False)
 
     # Provide two options for upscaling
-    # option = st.radio("Choose Upscaling Option:", ("Upscale Entire Image", "Upscale Selected Region"))
     option = st.selectbox("Choose Upscaling Option", ["Upscale Entire Image", "Upscale Selected Region"])
 
     # Upscale Entire Image Option
     if option == "Upscale Entire Image":
         if st.button("Upscale Entire Image"):
             try:
-                # Preprocess the image to reduce noise
-                preprocessed_image = preprocess_image(image)
-
                 # Convert preprocessed image to tensor for model input
                 preprocess = transforms.Compose([
                     transforms.Lambda(lambda img: img.convert('RGB') if img.mode == 'RGBA' else img),
                     transforms.ToTensor()])
-                image_tensor = preprocess(preprocessed_image).unsqueeze(0).to(device)
+                image_tensor = preprocess(image).unsqueeze(0).to(device)
 
                 # Feed the image to the model and run upscaling
                 model.feed_data({'lq': image_tensor})
@@ -70,17 +66,10 @@ if uploaded_file is not None:
                 output_image_tensor = model.get_current_visuals()['result'].squeeze().cpu()
                 output_image = transforms.ToPILImage()(output_image_tensor)
 
-                # Postprocess the output image to reduce color patches
-                #output_image = postprocess_image(output_image)
-
-                # Apply advanced postprocessing to reduce color artifacts
-                # output_image = advanced_postprocess_image(output_image)                  # --- Last Edit --- Good Results
-
             except RuntimeError:
                 st.write("##### ▲ An error occurred while processing the image. Please try with a smaller image.")
 
             else:
-                #display_width = calculate_display_dimensions_from_height(img_width, img_height, 2, 300)
                 st.image(output_image, caption="Upscaled Entire Image", width= 2 * img_width)
                 buf = io.BytesIO()
                 output_image.save(buf, format='PNG')
@@ -127,9 +116,6 @@ if uploaded_file is not None:
                         # Convert Numpy to  Image
                         roi_image = Image.fromarray(roi)
 
-                        # Preprocess the image to reduce noise
-                        preprocessed_image = preprocess_image(roi_image)
-
                         preprocess = transforms.Compose([
                             transforms.Lambda(lambda img: img.convert('RGB') if img.mode == 'RGBA' else img),
                             transforms.ToTensor()])
@@ -142,11 +128,7 @@ if uploaded_file is not None:
                         # Convert the output tensor to an image
                         output_image = transforms.ToPILImage()(model.get_current_visuals()['result'].squeeze().cpu())
 
-                        # Apply advanced postprocessing to reduce color artifacts
-                        output_image = advanced_postprocess_image(output_image)  
-
                     except RuntimeError:
-                        # st.error("An issue occurred while processing the image. Please try selecting a smaller region or use a different image.")
                         st.write("##### ▲ An error occurred while processing the image. Please try selecting a smaller region or use a different image.")
 
                     else:
